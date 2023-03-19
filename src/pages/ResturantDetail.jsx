0d48b6015df9fcd3996280a94914048cd326c6af
TheTRz112 <thetrz112@gmail.com>
2023-03-19 22:10:04 +0700

    19-3-2566

---
+++ src/pages/ResturantDetail.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ResturantDetail = () => {
+  return <div>ResturantDetail</div>;
+};
---
+++ src/pages/SeeAll.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeeAll = () => {
+  return <div>SeeAll</div>;
+};
+
+export default SeeAll;
