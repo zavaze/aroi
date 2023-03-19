4a4b74f3eabda678e7fdda6580915080aafd2e8a
TheTRz112 <thetrz112@gmail.com>
2023-03-19 23:05:07 +0700

    19/3-23.04

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import { AiOutlineSearch } from 'react-icons/ai';
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      className="navbar-container"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        position: 'sticky',
+        paddingRight: '10rem',
+      }}
+    >
+      <div>
+        <img src="/images/logo.png" alt="logo" />
+      </div>
+      <div>
+        <input type="text" placeholder="สถานที่ที่คุณต้องการไป.." />
+      </div>
+      <div>
+        <input type="text" placeholder="ร้านอาหาร/คาเฟ่" />
+        <button>
+          <AiOutlineSearch />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const resturants = [
+  {
+    id: '1',
+    img: '',
+    header: 'SteakHolder',
+    rating: '4.5',
+    cat: 'ranking',
+    review: '90',
+    description: 'สเต็ก, อาหารนานาชาติ',
+    date: 'ทุกวัน',
+    time: '16:00 - 20:00',
+    facilitate: ['ที่จอดรถ', 'Free WIFI', 'บัตรเครดิต', 'เดลิเวอรี่'],
+    seat: '41 - 80 ที่นั่ง',
+  },
+];
